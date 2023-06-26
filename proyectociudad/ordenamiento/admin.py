a63f621c8a6a8198ba8d78d571418464b3c3dbcb
rogercriollo <rlcriollo@utpl.edu.ec>
2023-06-26 17:41:24 -0500

    cambios sugeridos

--- proyectociudad/ordenamiento/admin.py
+++ proyectociudad/ordenamiento/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Parroquia,Barrio
 # Register your models here.
-admin.site.register(Parroquia)
-admin.site.register(Barrio)
+
+class ParroquiaAdmin(admin.ModelAdmin):
+    list_display = ('nombre',"tipo")
+    search_fields = ("nombre", "tipo")
+
+
+class BarrioAdmin(admin.ModelAdmin):
+    list_display = ('nombre',"numero_viviendas","numero_parques", "numero_edificios", "parroquia")
+    search_fields = ("nombre","numero_viviendas","numero_parques", "numero_edificios", "parroquia")
+     
+
+admin.site.register(Parroquia, ParroquiaAdmin)
+admin.site.register(Barrio, BarrioAdmin)
--- proyectociudad/ordenamiento/models.py
+++ proyectociudad/ordenamiento/models.py
@@ -2,20 +2,21 @@ from django.db import models
 
 # Create your models here.
 class Parroquia(models.Model):
+    options = (("tipo", "Rural"), ("tipo", "Úrbano"))
     nombre = models.CharField(max_length=100)
-    tipo = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100, choices=options)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} - {self.tipo}"
 
 class Barrio(models.Model):  
     nombre = models.CharField(max_length=100)
     numero_viviendas=models.IntegerField()
     numero_parques=models.IntegerField()
     numero_edificios=models.IntegerField()
-    parroquia=models.ForeignKey(Parroquia,on_delete=models.CASCADE, related_name="Barrios")
+    parroquia=models.ForeignKey(Parroquia,on_delete=models.CASCADE, related_name="barrios")
 
     def __str__(self):
-        return self.nombre
+        return  f"{self.nombre} - {self.parroquia}"
    
 
